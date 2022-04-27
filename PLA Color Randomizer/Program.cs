47e782c26874084b32b363c9fb6a6f521e2a3dae
BuildTools <44118666+hp3721@users.noreply.github.com>
2022-04-27 15:45:18 -0500

    Add project files.

---
+++ PLA Color Randomizer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using pkNX.Containers;
+using pkNX.Structures.FlatBuffers;
+
+namespace PLA_Color_Randomizer
+{
+    internal class Program
+    {
+        static readonly Random rand = new Random();
+        static readonly ContainerHandler ProgressTracker = new();
+        static readonly CancellationTokenSource TokenSource = new();
+        static List<Task> tasks = new List<Task>();
+        static string outDirPath;
+
+        static void Main(string[] args)
+        {
+            GetPacks(args[0]);
+        }
+
+        static void GetPacks(string dirPath)
+        {
+            outDirPath = Directory.CreateDirectory(Path.Combine(dirPath, "modified_pokemon")).FullName;
+
+            var paks = Directory.EnumerateFiles(dirPath, "*.gfpak").Where(z => Path.GetFileName(z).StartsWith("pm"));
+            foreach (var f in paks)
+            {
+                var pak = new GFPack(f);
+                RandomizeBaseLayerColors(ref pak, true);
+            }
+            Console.WriteLine("Waiting for all gfpak files to be exported");
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Saved modified gfpak files to {outDirPath}");
+        }
+
+        static void SaveGFPAK(ref GFPack pak)
+        {
+            Task task = pak.SaveAs(Path.Combine(outDirPath, $"{Path.GetFileNameWithoutExtension(pak.FilePath)}.gfpak"), ProgressTracker, TokenSource.Token);
+            task.Start();
+            tasks.Add(task);
+        }
+
+        static void RandomizeBaseLayerColors(ref GFPack pak, bool randShiny = false)
+        {
+            string trmtrFileName = $"{Path.GetFileNameWithoutExtension(pak.FilePath)}{(randShiny ? "_rare" : "")}.trmtr";
+            try
+            {
+                byte[] trmtrFile = pak.GetDataFileName(trmtrFileName);
+                trmtr8a trmtrFlatbuffer = FlatBufferConverter.DeserializeFrom<trmtr8a>(trmtrFile);
+                RandomizeBaseLayerColors(ref trmtrFlatbuffer);
+                pak.SetDataFileName(trmtrFileName, trmtrFlatbuffer.Write());
+                SaveGFPAK(ref pak);
+                Console.WriteLine(trmtrFileName);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return;
+            }
+        }
+
+        static void RandomizeBaseLayerColors(ref trmtr8a trmtrFlatbuffer)
+        {
+            foreach (var material in trmtrFlatbuffer.Materials)
+            {
+                foreach (var colorValue in material.ColorValues)
+                {
+                    if (!colorValue.ColorName.StartsWith("BaseColorLayer"))
+                        continue;
+
+                    colorValue.ColorValue.R = (float)rand.NextDouble();
+                    colorValue.ColorValue.G = (float)rand.NextDouble();
+                    colorValue.ColorValue.B = (float)rand.NextDouble();
+                }
+            }
+        }
+    }
+}
---
+++ PLA Color Randomizer/trmtrFlatbuffer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using FlatSharp.Attributes;
+using pkNX.Structures.FlatBuffers;
+
+namespace PLA_Color_Randomizer
+{
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtr8a
+    {
+        public byte[] Write() => FlatBufferConverter.SerializeFrom(this);
+
+        [FlatBufferItem(00)] public uint Field_00 { get; set; }
+        [FlatBufferItem(01)] public trmtrMaterial8a[] Materials { get; set; } = Array.Empty<trmtrMaterial8a>();
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrMaterial8a
+    {
+        [FlatBufferItem(00)] public string Name { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public trmtrShader8a[] Flags { get; set; } = Array.Empty<trmtrShader8a>();
+        [FlatBufferItem(02)] public trmtrMap8a[] Textures { get; set; } = Array.Empty<trmtrMap8a>();
+        [FlatBufferItem(03)] public trmtrSamplerState8a[] SamplerStates { get; set; } = Array.Empty<trmtrSamplerState8a>();
+        [FlatBufferItem(04)] public trmtrFloatValue8a[] FloatValues { get; set; } = Array.Empty<trmtrFloatValue8a>();
+        [FlatBufferItem(05)] public uint[] Field_05 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(06)] public uint[] Field_06 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(07)] public trmtrColorValue8a[] ColorValues { get; set; } = Array.Empty<trmtrColorValue8a>();
+        [FlatBufferItem(08)] public uint[] Field_08 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(09)] public trmtrIntValue8a[] IntValues { get; set; } = Array.Empty<trmtrIntValue8a>();
+        [FlatBufferItem(10)] public uint[] Field_10 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(11)] public uint[] Field_11 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(12)] public uint[] Field_12 { get; set; } = Array.Empty<uint>();
+        [FlatBufferItem(13)] public trmtrUnkTable8a Field_13 { get; set; }
+        [FlatBufferItem(14)] public trmtrUnkTable8a Field_14 { get; set; }
+        [FlatBufferItem(15)] public string AlphaType { get; set; } = string.Empty;
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrUnkTable8a
+    {
+        [FlatBufferItem(00)] public uint Field_00 { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrShader8a
+    {
+        [FlatBufferItem(00)] public string ShaderType { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public trmtrStringValue8a[] Flags { get; set; } = Array.Empty<trmtrStringValue8a>();
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrUnkValue8a
+    {
+        [FlatBufferItem(00)] public string UnkName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public uint UnkValue { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrColorValue8a
+    {
+        [FlatBufferItem(00)] public string ColorName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public trmtrRGBA8b ColorValue { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrFloatValue8a
+    {
+        [FlatBufferItem(00)] public string FloatName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public float FloatValue { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrIntValue8a
+    {
+        [FlatBufferItem(00)] public string IntName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public uint IntValue { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrStringValue8a
+    {
+        [FlatBufferItem(00)] public string StringName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public string StringValue { get; set; } = string.Empty;
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrMap8a
+    {
+        [FlatBufferItem(00)] public string MapName { get; set; } = string.Empty;
+        [FlatBufferItem(01)] public string MapFile { get; set; } = string.Empty;
+        [FlatBufferItem(02)] public uint MapSlot { get; set; }
+    }
+
+    [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
+    public class trmtrSamplerState8a
+    {
+        [FlatBufferItem(00)] public uint SamplerState0 { get; set; }
+        [FlatBufferItem(01)] public uint SamplerState1 { get; set; }
+        [FlatBufferItem(02)] public uint SamplerState2 { get; set; }
+        [FlatBufferItem(03)] public uint SamplerState3 { get; set; }
+        [FlatBufferItem(04)] public uint SamplerState4 { get; set; }
+        [FlatBufferItem(05)] public uint SamplerState5 { get; set; }
+        [FlatBufferItem(06)] public uint SamplerState6 { get; set; }
+        [FlatBufferItem(07)] public uint SamplerState7 { get; set; }
+        [FlatBufferItem(08)] public uint SamplerState8 { get; set; }
+        [FlatBufferItem(09)] public trmtrUVWrapMode8a RepeatU { get; set; }
+        [FlatBufferItem(10)] public trmtrUVWrapMode8a RepeatV { get; set; }
+        [FlatBufferItem(11)] public trmtrUVWrapMode8a Repeat2 { get; set; }
+        [FlatBufferItem(12)] public trmtrRGBA8b BorderColor { get; set; }
+        [FlatBufferItem(13)] public float MipMapBias { get; set; }
+    }
+
+    [FlatBufferEnum(typeof(uint))]
+    public enum trmtrUVWrapMode8a : uint
+    {
+        WRAP = 0,
+        CLAMP = 1,
+        MIRROR = 2,
+        BORDER = 3,
+        MIRROR_ONCE = 4,
+    }
+
+    [FlatBufferStruct]
+    public class trmtrRGBA8b
+    {
+        [FlatBufferItem(00)] public float R { get; set; }
+        [FlatBufferItem(01)] public float G { get; set; }
+        [FlatBufferItem(02)] public float B { get; set; }
+        [FlatBufferItem(03)] public float A { get; set; }
+    }
+
+}
